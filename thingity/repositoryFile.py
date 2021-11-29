ce44ab66c12e9fd593b90002ae5a22ccc415c82a
ian <ian@purplepip.com>
2021-11-29 19:23:07 +0000

    Allow things in things root and not in git repository

--- thingity/repositoryFile.py
+++ thingity/repositoryFile.py
@@ -1,23 +1,50 @@
 import subprocess
 import os
+from pathlib import Path
+
+
+def upFind(directory):
+    cwd = Path(".").resolve()
+    print(cwd)
+    candidate = Path(directory).resolve()
+    pathRoot = candidate.root
+    while len(candidate.name) > 1 and pathRoot != candidate.name:
+        print(candidate)
+        if candidate.parent.name == "things":
+            if candidate.is_relative_to(cwd):
+                return str(candidate.relative_to(cwd))
+            else:
+                return str(candidate)
+        else:
+            candidate = candidate.parent
+
+
+def gitRoot(directory, cmd):
+    # No need to change directory if just echo which is done from unit test
+    # when mock command is injected
+    kwargs = {}
+    if cmd[0] != "echo":
+        kwargs["cwd"] = directory
+
+    return (
+        subprocess.Popen(cmd, stdout=subprocess.PIPE, text=True, **kwargs)
+        .communicate()[0]
+        .rstrip()
+    )
 
 
 class RepositoryFile:
     def __init__(self, base: str, path="", cmd=["git", "rev-parse", "--show-toplevel"]):
         fullPath = base + "/" + path
         directory = os.path.dirname(base + "/" + path)
-        kwargs = {}
-        # No need to change directory if just echo which is done from unit test
-        # when mock command is injected
-        if cmd[0] != "echo":
-            kwargs["cwd"] = directory
 
-        self._root = (
-            subprocess.Popen(cmd, stdout=subprocess.PIPE, text=True, **kwargs)
-            .communicate()[0]
-            .rstrip()
-        )
+        self._root = upFind(directory)
+        if not self._root:
+            self._root = gitRoot(directory, cmd)
+
+        print(self._root)
         self._path = fullPath[len(self._root) + 1 :]
+        print(self._path)
 
     @property
     def root(self):
